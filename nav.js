75dd01ed630fc76dc9056a6c00ba8edeea1ce5c2
Omole-Ohonsi Peace <161709284+pomoleohonsi@users.noreply.github.com>
2025-01-10 16:41:54 +0100

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,9 @@
+function show_nav() {
+    var drop_down = document.getElementById("drop_down");
+    if(drop_down.style.display == "block"){
+        drop_down.style.display = "none";
+    }else{
+        drop_down.style.display = "block";
+    }
+
+}
